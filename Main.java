76f2aff382cd6a1b6d3edb7cc8e8f486c919fd10
vujcecel <vujcecel@mail.uc.edu>
2021-02-08 17:15:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+/*
+Lab 4
+Partners: Evan Vujcec and Sam Green
+Date: 08-02-2021
+*/
+
+import java.util.Random;
+
+class Main {
+  public static void main(String[] args) {
+
+    // Generate number
+    Random r = new Random();
+    int randNum = 1 + r.nextInt(50);
+
+    // Print number and countdown
+    System.out.printf("The Random number is: %d%n", randNum);
+    for (int x = randNum; x >= 0; x--) {
+      System.out.println(x);
+    }
+
+    // Print message
+    if (randNum <= 15)
+      System.out.println("Ahoy mateys!");
+    else if (randNum > 20 && randNum < 42)
+      System.out.println("Cannonball!");
+    else
+      System.out.println("Look, a sea shanty!");
+  }
+}
